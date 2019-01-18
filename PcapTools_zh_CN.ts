66918ab7a2b1826f3a2cc9c51d11a234d828a388
johNILES <2922381900@qq.com>
2019-01-18 12:46:07 +0800

    地点：105楼203 1、调整代码的文件结构； 2、将WpdPack文件夹移动到Git仓库中，进行管理； 3、调整部分方法的描述； 4、调整了Ui文件，当还没有确定界面版本；

---
+++ PcapTools_zh_CN.ts
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>PcapTest</name>
+    <message>
+        <location filename="Core/PcapTest.cpp" line="26"/>
+        <source>warning</source>
+        <translation type="unfinished">告警</translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.cpp" line="26"/>
+        <source>in working ,exit?</source>
+        <translation type="unfinished">正在抓包中，是否退出？</translation>
+    </message>
+</context>
+<context>
+    <name>PcapTestClass</name>
+    <message>
+        <location filename="Core/PcapTest.ui" line="14"/>
+        <source>通控器数据备份</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="37"/>
+        <source>GroupBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="55"/>
+        <source>网卡：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="99"/>
+        <source>端口号：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="121"/>
+        <source>1024</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="153"/>
+        <source>确定</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="166"/>
+        <source>开始抓包</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="184"/>
+        <source>文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="190"/>
+        <source>工具</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="196"/>
+        <source>帮助</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="207"/>
+        <location filename="Core/PcapTest.ui" line="210"/>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="215"/>
+        <source>设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="220"/>
+        <source>关于 抓包工具</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Core/PcapTest.ui" line="223"/>
+        <source>关于</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include "PcapTest.h"
-#include <QtWidgets/QApplication>
+#include "Core/PcapTest.h"
+#include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+    //安装翻译文件
+    QTranslator t_Translator;
+    if(t_Translator.load(a.applicationDirPath()+"/translations/"+qApp->applicationName()+"_zh_CN.qm"))
+    {
+        a.installTranslator(&t_Translator);
+    }
+
 	PcapTest w;
 	w.show();
 	return a.exec();
