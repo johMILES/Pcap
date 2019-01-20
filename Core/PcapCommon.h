06a752e74a30e3f51c9b03eef3c71c2609a7b0fb
无动于衷 <1918176667@qq.com>
2019-01-20 17:43:44 +0800

    地点：南京 1、修改编码规范 2、添加状态栏路径提示 3、设置默认保存文件路径

--- Core/PcapCommon.h
+++ Core/PcapCommon.h
@@ -16,28 +16,30 @@ class PcapCommon : public QObject
 	Q_OBJECT
 public:
 	PcapCommon();
-	PcapCommon(u_short port);
+    PcapCommon(unsigned short port);
 	virtual ~PcapCommon();
 
 	void winSocketInit();
 	//获取适配器
 	QVector<_DEVInfo> findAllDev();
-	//打开
+    //打开适配器，开始抓包
 	bool openCard(const _DEVInfo);
-	//关闭
+    //关闭抓包
 	void closeCard();
-	//设置端口
-	void SetPort(u_short port);
+    //设置过滤端口
+    void setPort(unsigned short port);
+    void setFilePath(QString path);
 
-	void readDatFile();
+    //读取抓包文件
+    void readDatFile(QString path);
 
-    QString m_SelectPath;   //先择保存文件路径
+    QString m_SelectPath;   //默认保存文件路径
 
 private:
 	pcap_if_t *m_pAlldevs;
 	pcap_if_t *m_pDevs;
 	pcap_t *m_pAHandle;
-	u_short p_Port;
+    unsigned short p_Port;
 
 	//适配器个数
 	int decCount;
@@ -53,10 +55,9 @@ private:
 	//打印pcap_if_t所有信息
 	void ifPcap_t(pcap_if_t*);
 	/* 将数字类型的IP地址转换成字符串类型的 */
-	char *iptos(u_long in);
+    char *iptos(unsigned long in);
 
-	//获取当前时间
-	QString getTime();
+    QString getTime();
 
 private slots:
 	void slot_RecvDataInfo(_MessageContent MsgCon, QByteArray payload);
--- Core/PcapThread.h
+++ Core/PcapThread.h
@@ -10,12 +10,12 @@ class PcapThread : public QThread
 	Q_OBJECT
 public:
 	PcapThread();
-	PcapThread(pcap_t *handle, u_short port);
+    PcapThread(pcap_t *handle, unsigned short port);
 	virtual ~PcapThread();
 
 	void run();
 
-	void Loop(const struct pcap_pkthdr *header, const u_char *pkt_data);
+    void Loop(const struct pcap_pkthdr *header, const unsigned char *pkt_data);
 
 	double getTimeDifference(long,long);
 
@@ -24,19 +24,19 @@ signals:
 
 private:
 	pcap_t *m_pDev;
-	u_short p_Port;
+    unsigned short p_Port;
 
-	_MessageContent TCP(const sniff_ip *ip, int size_ip, u_int len, const u_char *buffer, QByteArray &payload);
-	void UDP(const u_char *buffer);
+    _MessageContent TCP(const sniff_ip *ip, int size_ip, unsigned int len, const unsigned char *buffer, QByteArray &payload);
+    void UDP(const unsigned char *buffer);
 
 
 	//以16字节为单位打印数据：偏移十六进制ascii
-	void print_hex_ascii_line(const u_char *payload, int len, int offset);
+    void print_hex_ascii_line(const unsigned char *payload, int len, int offset);
 
 	/*
 	*打印包有效载荷数据（避免打印二进制数据）
 	*/
-	void print_payload(const u_char *payload, int len);
+    void print_payload(const unsigned char *payload, int len);
 	
 };
 
